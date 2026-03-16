6897caf4f2d46b8a15e243f5c2217d6e3b970217
Revandya Hendrico <144752445+ZinKx135@users.noreply.github.com>
2026-03-16 22:47:39 +0700

    Add files via upload

--- godot-game-space/Week 2/portal.gd
+++ godot-game-space/Week 2/portal.gd
@@ -1,5 +1,36 @@
 extends Area2D
 
-func _on_body_entered(body: CharacterBody2D) -> void:
-	if body.is_in_group("Player"):
-		body.set_position($Tujuan.global_position)
+@export var target_portal: NodePath
+
+var teleport_locked := false
+var last_body: Node = null
+
+
+func _ready():
+	$AnimatedSprite2D.play("Portal_idle")
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+
+
+func _on_body_entered(body):
+
+	if teleport_locked:
+		return
+
+	if body is CharacterBody2D:
+
+		var portal = get_node(target_portal)
+
+		# teleport player
+		body.global_position = portal.global_position + Vector2(0, 32)
+
+		# lock portal tujuan
+		portal.teleport_locked = true
+		portal.last_body = body
+
+
+func _on_body_exited(body):
+
+	if body == last_body:
+		teleport_locked = false
+		last_body = null
